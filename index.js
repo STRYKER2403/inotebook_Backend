47765389bd1a2e34ae536e67f2ddce79c77c46bc
SWAPNIL SRIVASTAVA <75637474+STRYKER2403@users.noreply.github.com>
2022-10-04 03:30:36 +0530

    After Edits

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const connecToMongo = require("./db");
 const express = require('express')
+var cors = require('cors')
 
 connecToMongo();
 
 const app = express()
 const port = 5000
 
+// Middleware to remove cors error
+app.use(cors())
 // Middleware to use req.body
-app.use(express.json())  
+app.use(express.json()) 
+
 
 // Available Routes
 app.use("/api/auth",require("./Routes/auth"))
 app.use("/api/notes",require("./Routes/notes"))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`iNotebook Backend listening on port ${port}`)
 })
